6268d52aacd2b1109b38fc75b92e090b1d4f12a3
Rqkko <rakko26@gmail.com>
2025-10-04 11:24:35 +0700

    fix: uri does not exist

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,10 +1,14 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-const client = new ApolloClient({
+const link = new HttpLink({
   uri: process.env.NEXT_PUBLIC_APPSYNC_URL,
   headers: {
     "x-api-key": process.env.NEXT_PUBLIC_APPSYNC_API_KEY ?? "",
   },
+});
+
+const client = new ApolloClient({
+  link,
   cache: new InMemoryCache(),
 });
 
